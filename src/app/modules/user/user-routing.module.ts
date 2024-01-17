fdcc60d72557500e54faf3a13686ca8c031646ff
Alin Chiperi <chiperialin@yahoo.com>
2024-01-17 12:55:50 +0200

    Finish reminder CRUD

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainUserComponent } from './main-user/main-user.component';
+import { RemindersComponent } from './reminders/reminders.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainUserComponent,
+  },
+  {
+    path: 'reminders',
+    component: RemindersComponent,
   },
 ];
 
